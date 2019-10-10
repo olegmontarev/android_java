4cebe489248d400d6a5522e0bf39056692488528
olegmont <olegmontarev@gmai.com>
2019-10-10 15:56:55 +0300

    homework

---
+++ app/src/main/java/com/geekbrains/a1l2_activity/DataClass.java
@@ -0,0 +1,11 @@
+package com.geekbrains.a1l2_activity;
+
+import android.graphics.drawable.Drawable;
+
+public class DataClass {
+    String text;
+
+    DataClass(String text) {
+        this.text = text;
+    }
+}
--- app/src/main/java/com/geekbrains/a1l2_activity/ViewWeather.java
+++ app/src/main/java/com/geekbrains/a1l2_activity/ViewWeather.java
@@ -5,18 +5,27 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ViewWeather extends AppCompatActivity {
 
     private TextView weather;
     private TextView additional;
-    private boolean wind;
+    RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         initViews();
+        initRecyclerView();
         Intent intent = getIntent();
         String city = intent.getStringExtra("city");
         boolean wind = intent.getBooleanExtra("wind", false);
@@ -31,6 +40,25 @@ public class ViewWeather extends AppCompatActivity {
     private void initViews() {
         weather = findViewById(R.id.textView);
         additional = findViewById(R.id.textView2);
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    private void initRecyclerView() {
+        DataClass[] data = new DataClass[] {
+                new DataClass("Weather at monday 10 degrees"),
+                new DataClass("Weather at tuesday 15 degrees"),
+                new DataClass("Weather at wednesday 17 degrees")
+        };
+
+        ArrayList<DataClass> list = new ArrayList<>(data.length);
+        list.addAll(Arrays.asList(data));
+
+        //GridLayoutManager layoutManager = new GridLayoutManager(getBaseContext(), 3);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
+        adapter = new RecyclerViewAdapter(list);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 
     private void showWeather(String city) {
@@ -67,5 +95,12 @@ public class ViewWeather extends AppCompatActivity {
         if (pressure) {
             additional.setText(R.string.checkPressure);
         }
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
