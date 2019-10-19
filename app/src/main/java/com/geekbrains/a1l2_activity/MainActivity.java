c837f7885ee6caf91a56abf5f8757b881d1792d4
olegmont <olegmontarev@gmai.com>
2019-10-19 13:40:19 +0300

    hw

--- app/src/main/java/com/geekbrains/a1l2_activity/MainActivity.java
+++ app/src/main/java/com/geekbrains/a1l2_activity/MainActivity.java
@@ -1,29 +1,21 @@
 package com.geekbrains.a1l2_activity;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
-
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-      private Spinner spinner;
+      TextInputEditText textInputEditText;
       private Button toSecondActivityBtn;
-      private CheckBox wind;
-      private CheckBox humidity;
-      private CheckBox pressure;
+      private CheckBox details;
       private static final String TAG = "Log";
-      String[] data = {"Moscow", "Berlin", "London"};
       String city = "default";
 
     @Override
@@ -33,104 +25,32 @@ public class MainActivity extends AppCompatActivity {
         initViews();
         setBehaviourForTo2ndActBtn();
         Toast.makeText(getBaseContext(), "onCreate", Toast.LENGTH_SHORT).show();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        final Spinner spinner = findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
-        spinner.setPrompt("Выберите город");
-        spinner.setSelection(2);
-        wind.setOnClickListener(new View.OnClickListener() {
+        details.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Просмотр скорости ветра", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Узнать ветер, влажность и давление", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-        humidity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Просмотр процента влажности", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        pressure.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Просмотр давления", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                city = spinner.getSelectedItem().toString();
-                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show(); }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) { }});}
-
+    }
             private void initViews() {
-        spinner = findViewById(R.id.spinner);
         toSecondActivityBtn = findViewById(R.id.toSecondActivityBtn);
-        wind = findViewById(R.id.checkBox);
-        humidity = findViewById(R.id.checkBox2);
-        pressure = findViewById(R.id.checkBox3);
+        textInputEditText = findViewById(R.id.textInput);
+        details = findViewById(R.id.checkBox);
     }
 
     private void setBehaviourForTo2ndActBtn() {
         toSecondActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                city = Objects.requireNonNull(textInputEditText.getText()).toString();
                 Intent intent = new Intent(MainActivity.this, ViewWeather.class);
                 intent.putExtra("city", city);
-                intent.putExtra("wind", wind.isChecked());
-                intent.putExtra("humidity", humidity.isChecked());
-                intent.putExtra("pressure", pressure.isChecked());
+                intent.putExtra("details", details.isChecked());
                 startActivity(intent);
                 finish();
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(getBaseContext(), "onStart", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "OnStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getBaseContext(), "onResume", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "OnResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getBaseContext(), "onPause", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "OnPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getBaseContext(), "onStop", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "OnStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getBaseContext(), "onDestroy", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "OnDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(getBaseContext(), "onRestart", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "OnRestart");
     }
 
 }
--- app/src/main/java/com/geekbrains/a1l2_activity/ViewWeather.java
+++ app/src/main/java/com/geekbrains/a1l2_activity/ViewWeather.java
@@ -5,96 +5,157 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ViewWeather extends AppCompatActivity {
 
-    private TextView weather;
-    private TextView additional;
-    RecyclerView recyclerView;
-    RecyclerViewAdapter adapter;
-
+    private final Handler handler = new Handler();
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+    private TextView cityTextView;
+    private TextView updatedTextView;
+    private TextView detailsTextView;
+    private TextView currentTemperatureTextView;
+    private TextView weatherIconTextView;
+    private boolean detail;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         initViews();
-        initRecyclerView();
         Intent intent = getIntent();
         String city = intent.getStringExtra("city");
-        boolean wind = intent.getBooleanExtra("wind", false);
-        boolean humidity = intent.getBooleanExtra("humidity", false);
-        boolean pressure = intent.getBooleanExtra("pressure", false);
-        showWeather(city);
-        showWind(wind);
-        showHumidity(humidity);
-        showPressure(pressure);
+        detail = intent.getBooleanExtra("details", false);
+        updateWeatherData(city);
     }
 
     private void initViews() {
-        weather = findViewById(R.id.textView);
-        additional = findViewById(R.id.textView2);
-        recyclerView = findViewById(R.id.recyclerView);
+        cityTextView = findViewById(R.id.city_field);
+        updatedTextView = findViewById(R.id.updated_field);
+        detailsTextView = findViewById(R.id.details_field);
+        currentTemperatureTextView = findViewById(R.id.current_temperature_field);
+        weatherIconTextView = findViewById(R.id.weather_icon);
     }
 
-    private void initRecyclerView() {
-        DataClass[] data = new DataClass[] {
-                new DataClass("Weather at monday 10 degrees"),
-                new DataClass("Weather at tuesday 15 degrees"),
-                new DataClass("Weather at wednesday 17 degrees")
-        };
-
-        ArrayList<DataClass> list = new ArrayList<>(data.length);
-        list.addAll(Arrays.asList(data));
-
-        //GridLayoutManager layoutManager = new GridLayoutManager(getBaseContext(), 3);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
-        adapter = new RecyclerViewAdapter(list);
-
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
+    private void updateWeatherData(final String city) {
+        new Thread() {
+            @Override
+            public void run() {
+                final JSONObject jsonObject = WeatherDataLoader.getJSONData(city);
+                if(jsonObject == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), R.string.place_not_found,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderWeather(jsonObject);
+                        }
+                    });
+                }
+            }
+        }.start();
     }
 
-    private void showWeather(String city) {
-        switch (city){
-            case "Moscow":
-                weather.setText(R.string.moscowWeather);
-                break;
-            case "Berlin":
-                weather.setText(R.string.berlinWeather);
-                break;
-            case "London":
-                weather.setText(R.string.londonWeather);
-                break;
-                default:
-                    weather.setText("Такого города нет в базе");
+    private void renderWeather(JSONObject jsonObject) {
+        Log.d(LOG_TAG, "json: " + jsonObject.toString());
+        try {
+            JSONObject details = jsonObject.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = jsonObject.getJSONObject("main");
+
+            setPlaceName(jsonObject);
+            if (detail) {
+            setDetails(details, main); }
+            setCurrentTemp(main);
+            setUpdatedText(jsonObject);
+            setWeatherIcon(details.getInt("id"),
+                    jsonObject.getJSONObject("sys").getLong("sunrise") * 1000,
+                    jsonObject.getJSONObject("sys").getLong("sunset") * 1000);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            Log.e(LOG_TAG, "One or more fields not found in the JSON data");
+        }
+    }
+
+    private void setPlaceName(JSONObject jsonObject) throws JSONException {
+        String cityText = jsonObject.getString("name").toUpperCase() + ", "
+                + jsonObject.getJSONObject("sys").getString("country");
+        cityTextView.setText(cityText);
+    }
+
+    private void setDetails(JSONObject details, JSONObject main) throws JSONException {
+        String detailsText = details.getString("description").toUpperCase() + "\n"
+                + "Humidity: " + main.getString("humidity") + "%" + "\n"
+                + "Pressure: " + main.getString("pressure") + "hPa";
+        detailsTextView.setText(detailsText);
+    }
+
+    private void setCurrentTemp(JSONObject main) throws JSONException {
+        String currentTextText = String.format(Locale.getDefault(), "%.2f",
+                main.getDouble("temp")) + "\u2103";
+        currentTemperatureTextView.setText(currentTextText);
+    }
+
+    private void setUpdatedText(JSONObject jsonObject) throws JSONException {
+        DateFormat dateFormat = DateFormat.getDateTimeInstance();
+        String updateOn = dateFormat.format(new Date(jsonObject.getLong("dt") * 1000));
+        String updatedText = "Last update: " + updateOn;
+        updatedTextView.setText(updatedText);
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+        int id = actualId / 100;
+        String icon = "";
+
+        if(actualId == 800) {
+            long currentTime = new Date().getTime();
+            if(currentTime >= sunrise && currentTime < sunset) {
+                icon = "\u2600";
+                //icon = getString(R.string.weather_sunny);
+            } else {
+                icon = getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch (id) {
+                case 2: {
+                    icon = getString(R.string.weather_thunder);
                     break;
+                }
+                case 3: {
+                    icon = getString(R.string.weather_drizzle);
+                    break;
+                }
+                case 5: {
+                    icon = getString(R.string.weather_rainy);
+                    break;
+                }
+                case 6: {
+                    icon = getString(R.string.weather_snowy);
+                    break;
+                }
+                case 7: {
+                    icon = getString(R.string.weather_foggy);
+                    break;
+                }
+                case 8: {
+                    icon = "\u2601";
+                    break;
+                }
+            }
         }
-
-    }
-
-    private void showWind(boolean wind){
-        if (wind) {
-            additional.setText(R.string.checkWind);
-        }
-    }
-
-    private void showHumidity(boolean humidity){
-        if (humidity) {
-            additional.setText(R.string.checkHumidity);
-        }
-    }
-
-    private void showPressure(boolean pressure){
-        if (pressure) {
-            additional.setText(R.string.checkPressure);
-        }
+        weatherIconTextView.setText(icon);
     }
 
     @Override
